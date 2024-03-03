aa5f35ebad02a281308ce6b56e062c1844dfc3dd
Mehmet Cetin <cetin@kinematiks.com>
2024-03-03 23:27:59 +0300

    asdg

---
+++ agent.go
@@ -0,0 +1,6 @@
+package agent2
+
+// agent has rsi, bb indicators
+// indicators sorted by their period asc as an array
+// agent has tp, sl, pos. expiry millis, bakoff millis
+// --dependent on position and trade implementation
