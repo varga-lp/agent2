a946c30ebc21a083e06aea6fa192c61179145fd2
Mehmet Cetin <cetin@kinematiks.com>
2024-03-04 17:33:28 +0300

    v0.0.1

--- agents.go
+++ agents.go
@@ -1,5 +1,12 @@
 package agent2
 
+import (
+	"fmt"
+	"sort"
+
+	"github.com/varga-lp/data/klines"
+)
+
 // agent has rsi, bb indicators
 // indicators sorted by their period asc as an array
 // both bb, rsi calc speed is similar, min period is
@@ -16,4 +23,125 @@ const (
 	Expiry ClosingReason = iota
 	StopLoss
 	TakeProfit
+	NoReason
 )
+
+const (
+	maxBBCount  = 5
+	maxRSICount = 5
+)
+
+type Agent struct {
+	Tpsl         *TPSL
+	Backoff      *Backoff
+	ExpiryMillis int64
+
+	Bbs  []*BB
+	Rsis []*RSI
+}
+
+func RandomAgent() *Agent {
+	bbMons, rsiMons := make(map[Monitor]struct{}), make(map[Monitor]struct{})
+
+	ag := &Agent{
+		Tpsl:         RandomTPSL(),
+		Backoff:      RandomBackoff(),
+		ExpiryMillis: randExpiry(),
+		Bbs:          make([]*BB, 0, maxBBCount),
+		Rsis:         make([]*RSI, 0, maxRSICount),
+	}
+
+	for i := 0; i < maxBBCount; i++ {
+		bb := RandomBB()
+
+		if _, ok := bbMons[bb.Mon]; !ok {
+			bbMons[bb.Mon] = struct{}{}
+
+			ag.Bbs = append(ag.Bbs, bb)
+		}
+	}
+	for i := 0; i < maxRSICount; i++ {
+		rsi := RandomRSI()
+
+		if _, ok := rsiMons[rsi.Mon]; !ok {
+			rsiMons[rsi.Mon] = struct{}{}
+
+			ag.Rsis = append(ag.Rsis, rsi)
+		}
+	}
+	sort.Slice(ag.Bbs[:], func(i, j int) bool {
+		return ag.Bbs[i].Period < ag.Bbs[j].Period
+	})
+	sort.Slice(ag.Rsis[:], func(i, j int) bool {
+		return ag.Rsis[i].Period < ag.Rsis[j].Period
+	})
+
+	return ag
+}
+
+const (
+	minActivationKlineLength = 250
+)
+
+var (
+	ErrKlinesAreBelowMinActivationKlineLength = fmt.Errorf("kline length is below min activation kline length 250")
+	ErrPositionCantBeNilForClose              = fmt.Errorf("position can't be nil for close pos")
+)
+
+func (ag *Agent) OpenPos(klns1 []klines.Kline, klns2 []klines.Kline, lastTrade *Trade) (bool, error) {
+	// check backoff
+	if !ag.Backoff.TradeAllowed(lastTrade) {
+		return false, nil
+	}
+	klns1Len, klns2Len := len(klns1), len(klns2)
+
+	if klns1Len < minActivationKlineLength || klns2Len < minActivationKlineLength {
+		return false, ErrKlinesAreBelowMinActivationKlineLength
+	}
+
+	// check rsi indicators first as its faster than bb
+	for _, rsi := range ag.Rsis {
+		active, err := rsi.Active(klns1[klns1Len-rsi.Period:], klns2[klns2Len-rsi.Period:])
+		if err != nil {
+			return false, err
+		}
+		if !active {
+			return false, nil
+		}
+	}
+	// check bb indicators
+	for _, bb := range ag.Bbs {
+		active, err := bb.Active(klns1[klns1Len-bb.Period:], klns2[klns2Len-bb.Period:])
+		if err != nil {
+			return false, err
+		}
+		if !active {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func (ag *Agent) ClosePos(pos *Position, closeLong klines.Kline, closeShort klines.Kline) (bool, ClosingReason, error) {
+	if pos == nil {
+		return false, NoReason, ErrPositionCantBeNilForClose
+	}
+
+	// check sl
+	if clos, err := ag.Tpsl.SLNetClose(pos, closeLong, closeShort); err != nil {
+		return false, NoReason, err
+	} else if clos {
+		return true, StopLoss, nil
+	}
+	// check tp
+	if clos, err := ag.Tpsl.TPNetClose(pos, closeLong, closeShort); err != nil {
+		return false, NoReason, err
+	} else if clos {
+		return true, TakeProfit, nil
+	}
+	// check expiry
+	if pos.Expired(ag.ExpiryMillis) {
+		return true, Expiry, nil
+	}
+	return false, NoReason, nil
+}
---
+++ agents_test.go
@@ -0,0 +1,214 @@
+package agent2
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRandomAgent_BBLen(t *testing.T) {
+	for i := 0; i < 10_000; i++ {
+		bbLen := len(RandomAgent().Bbs)
+
+		if bbLen < 1 || bbLen > maxBBCount {
+			t.Errorf("bbLen is outside of boundries")
+		}
+	}
+}
+
+func TestRandomAgent_RSILen(t *testing.T) {
+	for i := 0; i < 10_000; i++ {
+		rsiLen := len(RandomAgent().Rsis)
+
+		if rsiLen < 1 || rsiLen > maxRSICount {
+			t.Errorf("rsiLen is outside of boundries")
+		}
+	}
+}
+
+func TestRandomAgent_BBSorted(t *testing.T) {
+	for i := 0; i < 10_000; i++ {
+		ag := RandomAgent()
+
+		lastPeriod := 0
+		for _, bb := range ag.Bbs {
+			if bb.Period < lastPeriod {
+				t.Errorf("bbs are not sorted according to period")
+			}
+			lastPeriod = bb.Period
+		}
+	}
+}
+
+func TestRandomAgent_RSISorted(t *testing.T) {
+	for i := 0; i < 10_000; i++ {
+		ag := RandomAgent()
+
+		lastPeriod := 0
+		for _, rsi := range ag.Rsis {
+			if rsi.Period < lastPeriod {
+				t.Errorf("rsis are not sorted according to period")
+			}
+			lastPeriod = rsi.Period
+		}
+	}
+}
+
+func TestOpenPos_RaiseError_WithLowNumberOfKlines(t *testing.T) {
+	ag := RandomAgent()
+
+	_, err := ag.OpenPos(dummyKlines(minActivationKlineLength-1), dummyKlines(minActivationKlineLength-1), nil)
+	if err != ErrKlinesAreBelowMinActivationKlineLength {
+		t.Errorf("expected %v error but raised %v", ErrKlinesAreBelowMinActivationKlineLength, err)
+	}
+}
+
+func TestOpenPos_RaiseNoError_WithValidArgs_NoTrade(t *testing.T) {
+	ag := RandomAgent()
+
+	_, err := ag.OpenPos(dummyKlines(250), dummyKlines(250), nil)
+	if err != nil {
+		t.Errorf("expected no error but received %v", err)
+	}
+}
+
+func TestOpenPos_RaiseNoError_WithValidArgs_Trade(t *testing.T) {
+	ag := RandomAgent()
+	tr := &Trade{
+		CloseTime: time.Now().UnixMilli(),
+	}
+
+	_, err := ag.OpenPos(dummyKlines(250), dummyKlines(250), tr)
+	if err != nil {
+		t.Errorf("expected no error but received %v", err)
+	}
+}
+
+func TestOpenPos_SingleIndicator_Active(t *testing.T) {
+	ag := RandomAgent()
+	ag.Bbs = make([]*BB, 0)
+
+	rsi := RandomRSI()
+	rsi.TargetVal = 10.0
+	rsi.ValuePos = Above
+	rsi.Period = 250
+	ag.Rsis = []*RSI{rsi}
+
+	open, _ := ag.OpenPos(dummyKlines(250), dummyKlines(250), nil)
+	if !open {
+		t.Errorf("expected openpos to return true, returned false")
+	}
+}
+
+func TestOpenPos_SingleIndicator_NonActive(t *testing.T) {
+	ag := RandomAgent()
+	ag.Bbs = make([]*BB, 0)
+
+	rsi := RandomRSI()
+	rsi.TargetVal = 10.0
+	rsi.ValuePos = Below
+	rsi.Period = 250
+	ag.Rsis = []*RSI{rsi}
+
+	open, _ := ag.OpenPos(dummyKlines(250), dummyKlines(250), nil)
+	if open {
+		t.Errorf("expected openpos to return false, returned true")
+	}
+}
+
+func TestOpenPos_SingleIndicator_OneActiveOneInactive(t *testing.T) {
+	ag := RandomAgent()
+
+	bb := RandomBB()
+	bb.Period = 250
+	bb.Line = Upper
+	bb.ValuePos = Above
+	ag.Bbs = []*BB{bb}
+
+	rsi := RandomRSI()
+	rsi.TargetVal = 10.0
+	rsi.ValuePos = Above
+	rsi.Period = 250
+	ag.Rsis = []*RSI{rsi}
+
+	open, _ := ag.OpenPos(dummyKlines(250), dummyKlines(250), nil)
+	if open {
+		t.Errorf("expected openpos to return false, returned true")
+	}
+}
+
+func TestClosePos_RaiseWithNilPos(t *testing.T) {
+	ag := RandomAgent()
+	kln1, kln2 := dummyKlines(1)[0], dummyKlines(1)[0]
+
+	_, _, err := ag.ClosePos(nil, kln1, kln2)
+	if err != ErrPositionCantBeNilForClose {
+		t.Errorf("expected closepos to raise %v, returned %v",
+			ErrPositionCantBeNilForClose, err)
+	}
+}
+
+func TestClosePos_StopLoss(t *testing.T) {
+	kln1O, kln2O := dummyKlines(1)[0], dummyKlines(1)[0]
+	kln1C, kln2C := dummyKlines(1)[0], dummyKlines(1)[0]
+
+	kln1O.Close = 1.0
+	kln1C.Close = 1.0
+	kln2O.Close = 1.0
+	kln2C.Close = 1.5
+
+	p, _ := NewPosition(kln1O, kln2O)
+	ag := RandomAgent()
+
+	clos, reason, _ := ag.ClosePos(p, kln1C, kln2C)
+	if !clos {
+		t.Errorf("expected close to be true")
+	}
+	if reason != StopLoss {
+		t.Errorf("expected reason to be stop loss")
+	}
+}
+
+func TestClosePos_TakeProfit(t *testing.T) {
+	kln1O, kln2O := dummyKlines(1)[0], dummyKlines(1)[0]
+	kln1C, kln2C := dummyKlines(1)[0], dummyKlines(1)[0]
+
+	kln1O.Close = 1.0
+	kln1C.Close = 1.0
+	kln2O.Close = 1.0
+	kln2C.Close = 0.9
+
+	p, _ := NewPosition(kln1O, kln2O)
+	ag := RandomAgent()
+
+	clos, reason, _ := ag.ClosePos(p, kln1C, kln2C)
+	if !clos {
+		t.Errorf("expected close to be true")
+	}
+	if reason != TakeProfit {
+		t.Errorf("expected reason to be take profit")
+	}
+}
+
+func TestClosePos_Expiry(t *testing.T) {
+	kln1O, kln2O := dummyKlines(1)[0], dummyKlines(1)[0]
+	kln1C, kln2C := dummyKlines(1)[0], dummyKlines(1)[0]
+
+	kln1O.Close = 1.0
+	kln1C.Close = 1.0
+	kln2O.Close = 1.0
+	kln2C.Close = 1.0
+
+	kln1O.CloseTime = time.Now().UnixMilli() - 10_000
+
+	p, _ := NewPosition(kln1O, kln2O)
+	ag := RandomAgent()
+	ag.ExpiryMillis = 9_999
+
+	clos, reason, _ := ag.ClosePos(p, kln1C, kln2C)
+	if !clos {
+		t.Errorf("expected close to be true")
+	}
+	if reason != Expiry {
+		t.Errorf("expected reason to be expiry")
+	}
+}
