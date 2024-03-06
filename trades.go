cf45e818a601b2b3d7d2a8922f0b4b3ed0ffd460
Mehmet Cetin <cetin@kinematiks.com>
2024-03-06 10:27:06 +0300

    asdg

--- trades.go
+++ trades.go
@@ -2,21 +2,23 @@ package agent2
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/varga-lp/data/klines"
 )
 
 type Trade struct {
-	OpenTime     int64
-	CloseTime    int64
-	Reason       ClosingReason
-	NetProfit    float64
-	DurationSecs int64
+	OpenTime     int64         `json:"open_time"`
+	CloseTime    int64         `json:"close_time"`
+	Reason       ClosingReason `json:"reason"`
+	NetProfit    float64       `json:"net_profit"`
+	DurationSecs int64         `json:"duration_scs"`
 }
 
 var (
 	ErrPositionCantBeNilForTrade      = fmt.Errorf("position can't be nil when creating a trade")
 	ErrPositionCloseIsNotLTECloseTime = fmt.Errorf("pos.Long.CloseTime should be less than equal to longClose.CloseTime")
+	ErrBucketEndTimeIsNotGTStartTime  = fmt.Errorf("bucket end time is not gt than start time")
 )
 
 func NewTrade(pos *Position, cr ClosingReason, longClose klines.Kline, shortClose klines.Kline) (*Trade, error) {
@@ -39,4 +41,75 @@ func NewTrade(pos *Position, cr ClosingReason, longClose klines.Kline, shortClos
 func (tr *Trade) String() string {
 	return fmt.Sprintf("[trd] OT=%d, CT=%d, DSecs=%d, Net=%.2f, R=%s",
 		tr.OpenTime, tr.CloseTime, tr.DurationSecs, tr.NetProfit, tr.Reason)
+}
+
+type Bucket struct {
+	StartTime int64    `json:"start_time"`
+	EndTime   int64    `json:"end_time"`
+	Trades    []*Trade `json:"trades"`
+}
+
+func NewBucket(startTime int64, endTime int64) (*Bucket, error) {
+	if !(endTime > startTime) {
+		return nil, ErrBucketEndTimeIsNotGTStartTime
+	}
+
+	return &Bucket{
+		StartTime: startTime,
+		EndTime:   endTime,
+		Trades:    make([]*Trade, 0),
+	}, nil
+}
+
+func (bu *Bucket) LastTrade() *Trade {
+	tLen := len(bu.Trades)
+
+	if tLen == 0 {
+		return nil
+	}
+	return bu.Trades[tLen-1]
+}
+
+func (bu *Bucket) AppendTrade(pos *Position, cr ClosingReason, longClose klines.Kline, shortClose klines.Kline) error {
+	trade, err := NewTrade(pos, cr, longClose, shortClose)
+	if err != nil {
+		return err
+	}
+
+	bu.Trades = append(bu.Trades, trade)
+	return nil
+}
+
+const (
+	dayLenMillis = float64(24 * 60 * 60 * 1_000)
+)
+
+func (bu *Bucket) HitRatio() float64 {
+	if len(bu.Trades) == 0 {
+		return 0.0
+	}
+
+	profitableTrades := 0.0
+	for _, tr := range bu.Trades {
+		if tr.NetProfit > 0.0 {
+			profitableTrades++
+		}
+	}
+
+	hr := profitableTrades / float64(len(bu.Trades))
+	return roundTo4d(hr)
+}
+
+func (bu *Bucket) ProfitPerDay() float64 {
+	days, totalProfit := float64(bu.EndTime-bu.StartTime)/dayLenMillis, 0.0
+	for _, tr := range bu.Trades {
+		totalProfit += tr.NetProfit
+	}
+
+	ppd := totalProfit / days
+	return roundTo4d(ppd)
+}
+
+func roundTo4d(val float64) float64 {
+	return math.Round(val*10_000.0) / 10_000.0
 }
--- trades_test.go
+++ trades_test.go
@@ -105,3 +105,108 @@ func TestNewTrade_String(t *testing.T) {
 		t.Errorf("string %s is not expected %s", tr.String(), expected)
 	}
 }
+
+func TestRoundTo4d(t *testing.T) {
+	expected := 0.3333
+	r := roundTo4d(float64(1.0) / float64(3.0))
+
+	if r != expected {
+		t.Errorf("expected float %.4f is not equal to r %.4f", expected, r)
+	}
+}
+
+func TestNewBucket_InvalidArgs(t *testing.T) {
+	if _, err := NewBucket(2, 1); err != ErrBucketEndTimeIsNotGTStartTime {
+		t.Errorf("expected error not raised")
+	}
+}
+
+func TestNewBucket_WithValidArgs(t *testing.T) {
+	_, err := NewBucket(1, 2)
+	if err != nil {
+		t.Errorf("no error expected but raised")
+	}
+}
+
+func TestBucket_LastTrade_EmptyTrades(t *testing.T) {
+	b, _ := NewBucket(1, 2)
+
+	if b.LastTrade() != nil {
+		t.Errorf("expected nil trade")
+	}
+}
+
+func TestBucket_LastTrade_WithTrades(t *testing.T) {
+	pos1, _ := NewPosition(dummyKlines(1)[0], dummyKlines(1)[0])
+	pos2, _ := NewPosition(dummyKlines(2)[1], dummyKlines(2)[1])
+
+	tr1, _ := NewTrade(pos1, NoReason, dummyKlines(2)[1], dummyKlines(2)[1])
+	tr2, _ := NewTrade(pos2, NoReason, dummyKlines(3)[2], dummyKlines(3)[2])
+	tr2.Reason = Expiry
+
+	bu, _ := NewBucket(1, 2)
+	bu.Trades = append(bu.Trades, tr1)
+	bu.Trades = append(bu.Trades, tr2)
+
+	if bu.LastTrade().Reason != Expiry {
+		t.Errorf("unexpected reason")
+	}
+}
+
+func TestBucket_AppendTrade(t *testing.T) {
+	pos1, _ := NewPosition(dummyKlines(1)[0], dummyKlines(1)[0])
+	pos2, _ := NewPosition(dummyKlines(2)[1], dummyKlines(2)[1])
+
+	bu, _ := NewBucket(1, 2)
+	err := bu.AppendTrade(pos1, TakeProfit, dummyKlines(2)[1], dummyKlines(2)[1])
+	if err != nil {
+		t.Errorf("expected no error, but raised")
+	}
+
+	err = bu.AppendTrade(pos2, StopLoss, dummyKlines(3)[2], dummyKlines(3)[2])
+	if err != nil {
+		t.Errorf("expected no error, but raised")
+	}
+
+	if bu.LastTrade().Reason != StopLoss {
+		t.Errorf("unexpected reason")
+	}
+}
+
+func TestBucket_HitRatio_NoTrades(t *testing.T) {
+	bu, _ := NewBucket(1, 2)
+	if bu.HitRatio() != 0.0 {
+		t.Errorf("unpexpected hit ratio")
+	}
+}
+
+func TestBucket_HitRatio_WithTrades(t *testing.T) {
+	bu, _ := NewBucket(1, 2)
+	bu.Trades = append(bu.Trades, &Trade{NetProfit: -0.1})
+	bu.Trades = append(bu.Trades, &Trade{NetProfit: 0.5})
+	bu.Trades = append(bu.Trades, &Trade{NetProfit: 0.8})
+
+	expected := 0.6666
+	if bu.HitRatio() != expected {
+		t.Errorf("unexpected hit ratio")
+	}
+}
+
+func TestBucket_ProfitPerDay_NoTrades(t *testing.T) {
+	bu, _ := NewBucket(1, 2)
+	if bu.ProfitPerDay() != 0.0 {
+		t.Errorf("unexpected profit per day")
+	}
+}
+
+func TestBucket_ProfitPerDay_WithTrades(t *testing.T) {
+	bu, _ := NewBucket(0, int64(dayLenMillis)*2)
+	bu.Trades = append(bu.Trades, &Trade{NetProfit: 10.0})
+	bu.Trades = append(bu.Trades, &Trade{NetProfit: 20.0})
+	bu.Trades = append(bu.Trades, &Trade{NetProfit: -10.0})
+
+	expected := 10.0000
+	if bu.ProfitPerDay() != expected {
+		t.Errorf("unexpected profit per day")
+	}
+}
