43f1ba1280142376659a01da19fb881dd94c92fb
Mehmet Cetin <cetin@kinematiks.com>
2024-06-28 23:14:08 +0300

    fixes min ind. check

--- agents.go
+++ agents.go
@@ -44,9 +44,6 @@ func (cr ClosingReason) String() string {
 const (
 	maxBBCount  = 5
 	maxRSICount = 5
-
-	minBBCount  = 2
-	minRSICount = 2
 )
 
 type Agent struct {
@@ -85,7 +82,7 @@ func RandomAgent() *Agent {
 		Rsis:         make([]*RSI, 0, maxRSICount),
 	}
 
-	for i := minBBCount; i < maxBBCount; i++ {
+	for i := 0; i < maxBBCount; i++ {
 		bb := RandomBB()
 
 		if _, ok := bbMons[bb.Mon]; !ok {
@@ -94,7 +91,7 @@ func RandomAgent() *Agent {
 			ag.Bbs = append(ag.Bbs, bb)
 		}
 	}
-	for i := minRSICount; i < maxRSICount; i++ {
+	for i := 0; i < maxRSICount; i++ {
 		rsi := RandomRSI()
 
 		if _, ok := rsiMons[rsi.Mon]; !ok {
